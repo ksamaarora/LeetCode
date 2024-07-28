7b0bdd4bc6093fe596057735a14e21098c31be3a
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-07-29 01:55:25 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (27.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1;        // Base case: x^0 = 1
-        if (n < 0) {
-            if (n == INT_MIN) {
-                n = INT_MAX; // Convert n to INT_MAX instead of directly negating it
-                x = 1 / x;
-                return x * myPow(x, n); // Reduce the problem by one multiplication
-            } else {
-                x = 1 / x;
-                n = -n;
+        if (n==0) return 1; //Base case: x^0 = 1
+        if (n<0){
+            if (n==INT_MIN){ //for e.g. this -2,147,483,648 which gave an error if i dont do like this
+                n=INT_MAX; //Convert n to INT_MAX instead of directly negating it
+                x=1/x;
+                return x*myPow(x,n); 
+            }
+            else{
+                x=1/x;
+                n=-n;
             }
         }
-        if (n % 2 == 0) {
-            double half = myPow(x, n / 2);
-            return half * half;      // x^n = (x^(n/2)) * (x^(n/2))
+        if (n%2==0){ // this is imp orelse time exceeded
+            double half=myPow(x,n/2);
+            return half*half;
         } else {
-            return x * myPow(x, n - 1); // x^n = x * x^(n-1)
+            return x*myPow(x,n-1); // x^n = x * x^(n-1)
         }
     }
 };
