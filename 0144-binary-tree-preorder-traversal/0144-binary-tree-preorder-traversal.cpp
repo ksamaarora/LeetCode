b6fa81e07b1e855ebcbdfb826dc8b83074270347
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-09-09 23:52:28 +0530

    Time: 3 ms (33.07%), Space: 11.9 MB (5.00%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,36 +12,13 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> preorder;
-        if (root == nullptr) return preorder;
-
-        stack<TreeNode*> st;
-        st.push(root);
-
-        while (!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-            preorder.push_back(node->val);
-            // Push right child first to ensure left is processed first
-            if (node->right) st.push(node->right);
-            if (node->left) st.push(node->left);
-        }
-        return preorder;
+        vector<int> result;
+        if(root==nullptr) return result; // empty vector (null) returned 
+        result.push_back(root->val);
+        vector<int> left=preorderTraversal(root->left);
+        vector<int> right=preorderTraversal(root->right);
+        result.insert(result.end(), left.begin(), left.end());
+        result.insert(result.end(), right.begin(), right.end());
+        return result;
     }
 };
-
-// Method 1: Using Recursion
-// class Solution {
-// public:
-//     vector<int> preorderTraversal(TreeNode* root) {
-//         vector<int> result;
-//         if (root == nullptr) return result;
-//         result.push_back(root->val);// Visit the root node
-//         vector<int> left = preorderTraversal(root->left);//Traverse the left subtree
-//         vector<int> right = preorderTraversal(root->right); //Traverse the right subtree
-//         result.insert(result.end(), left.begin(), left.end());//Add left subtree values
-//         result.insert(result.end(), right.begin(), right.end());//Add right subtree values
-//         return result;
-//     }
-// };
-
