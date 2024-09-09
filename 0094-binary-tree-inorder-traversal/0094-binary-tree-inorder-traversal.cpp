638e5f25d3224dd9be5c743288eab6283c8cc701
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-09-10 00:56:53 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (5.59%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,43 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// Method 2: Using Stack
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-        vector<int> inorder;
-        
-        while (true) {
-            if (node != NULL) {
-                st.push(node);
-                node = node->left;
-            } else {
-                if (st.empty() == true) break;
-                node = st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node = node->right;
-            }
-        }
-        
-        return inorder;
+        vector<int> result;
+        if(root==nullptr) return result;
+        vector<int> left=inorderTraversal(root->left);
+        result.insert(result.end(), left.begin(), left.end());
+        result.push_back(root->val);
+        vector<int>right=inorderTraversal(root->right);
+        result.insert(result.end(), right.begin(), right.end());
+        return result;
     }
 };
-
-// Method 1: Using recursion
-// class Solution {
-// public:
-//     vector<int> inorderTraversal(TreeNode* root){
-//         vector<int> result;
-//         if(root==nullptr){return result;}
-//         vector<int> left=inorderTraversal(root->left);
-//         result.insert(result.end(), left.begin(), left.end());
-//         result.push_back(root->val);
-//         vector<int> right=inorderTraversal(root->right);
-//         result.insert(result.end(), right.begin(), right.end());
-//         return result;
-//     }
-// };
