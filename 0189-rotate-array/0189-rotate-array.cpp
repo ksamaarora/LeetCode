f76a6fbed86973926626c61ef022f958dbf82167
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-09-17 15:00:58 +0530

    Time: 17 ms (80.96%), Space: 27.5 MB (83.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n; // Ensure k is within bounds
-        vector<int> temp;
+//         int n = nums.size();
+//         k = k % n; // Ensure k is within bounds
+//         vector<int> temp;
 
-        // Store the last k elements in the temp vector
-        for (int i = n - k; i < n; i++) {
-            temp.push_back(nums[i]);
-        }
+//         // Store the last k elements in the temp vector
+//         for (int i = n - k; i < n; i++) {
+//             temp.push_back(nums[i]);
+//         }
 
-        // Shift the first n - k elements to the right
-        for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
+//         // Shift the first n - k elements to the right
+//         for (int i = n - k - 1; i >= 0; i--) {
+//             nums[i + k] = nums[i];
+//         }
 
-        // Move elements from temp to the beginning of nums
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
-        }
+//         // Move elements from temp to the beginning of nums
+//         for (int i = 0; i < k; i++) {
+//             nums[i] = temp[i];
+//         }
+        
+        
+        // Optimal Solution
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
