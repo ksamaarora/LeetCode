b0f0117ce23c234946dafe95901bca08c134f406
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-09-29 02:17:37 +0530

    Time: 576 ms (93.62%), Space: 299.8 MB (59.46%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -12,27 +12,44 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         // Method 1: Brute force TC:O(N+N/2) SC:O(1)
-        if(head == NULL || head->next == NULL) return NULL;
-        int index_end = 0;
-        ListNode* temp = head;
-        while(temp != NULL) {
-            temp = temp->next;
-            index_end++;
-        }
-        int index = index_end / 2;  // Middle index
+//         if(head == NULL || head->next == NULL) return NULL;
+//         int index_end = 0;
+//         ListNode* temp = head;
+//         while(temp != NULL) {
+//             temp = temp->next;
+//             index_end++;
+//         }
+//         int index = index_end / 2;  // Middle index
 
-        temp = head;
-        for(int i = 0; i < index - 1; i++) {
-            temp = temp->next;
-        }
+//         temp = head;
+//         for(int i = 0; i < index - 1; i++) {
+//             temp = temp->next;
+//         }
 
-        ListNode* deleteNode = temp->next;  // Node to be deleted
-        temp->next = deleteNode->next;      // Bypass the middle node
-        delete deleteNode;                  // Free the memory
+//         ListNode* deleteNode = temp->next;  // Node to be deleted
+//         temp->next = deleteNode->next;      // Bypass the middle node
+//         delete deleteNode;                  // Free the memory
 
-        return head;
+//         return head;
         
         // Method 2: Optimized method
-        
+        if(head==NULL || head->next==NULL) return NULL;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* prev=NULL;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            prev=slow;
+            slow=slow->next;
+        }
+        if(slow==head){
+            head=head->next;
+            delete slow;
+            return head;
+        }
+        ListNode* deleteNode=prev->next;
+        prev->next=slow->next;
+        delete deleteNode;
+        return head;
     }
 };
