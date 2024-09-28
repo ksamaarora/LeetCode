2b6eed55e1e70ad461732cb512d7faad6043993d
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-09-28 13:40:10 +0530

    Time: 172 ms (50.97%), Space: 123.2 MB (42.47%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,23 +10,64 @@
  */
 class Solution {
 public:
+    // Method 2: Tortoise and Hare method - IMP
+    ListNode* reverseLL(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* newHead = reverseLL(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+
     bool isPalindrome(ListNode* head) {
-        ListNode* temp=head;
-        stack<int> st; // LIFO
-        while(temp!=NULL){
-            st.push(temp->val);
-            temp=temp->next;
+        if(head == NULL || head->next == NULL) return true;  // Handle edge cases
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Tortoise and Hare method to find the middle of the list
+        while(fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        temp=head;
-        while(temp!=NULL){
-            if(temp->val==st.top()){
-                st.pop();
-                temp=temp->next;
-            }
-            else{
+
+        // Reverse the second half of the linked list
+        ListNode* newHead = reverseLL(slow->next);
+
+        ListNode* first = head;
+        ListNode* second = newHead;
+
+        // Compare both halves
+        while(second != NULL) {
+            if(first->val != second->val) {
+                reverseLL(newHead);  // Restore the list before returning false
                 return false;
             }
+            first = first->next;
+            second = second->next;
         }
+
+        reverseLL(newHead);  // Restore the reversed second half
         return true;
+        
+        // Method 1: Using stack DS TC:O(2N) SC:O(N)
+        // ListNode* temp=head;
+        // stack<int> st; // LIFO
+        // while(temp!=NULL){
+        //     st.push(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // while(temp!=NULL){
+        //     if(temp->val==st.top()){
+        //         st.pop();
+        //         temp=temp->next;
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
