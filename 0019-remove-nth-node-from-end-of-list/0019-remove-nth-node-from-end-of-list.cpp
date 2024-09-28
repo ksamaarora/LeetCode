2bd8ac0ba191292909c8cb1e1fac96d579ef2a15
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-09-28 23:52:59 +0530

    Time: 4 ms (56.25%), Space: 14.7 MB (35.97%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,31 +11,51 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        // Method 1: Brute force method TC:O(len)+O(len-n+1) worst case O(2N) and SC:O(1)
+        // if(head==NULL || head->next==NULL) return NULL;
+        // int cnt=0;
+        // ListNode* temp=head;
+        // while(temp!=NULL){ 
+        //     temp=temp->next;
+        //     cnt++;
+        // }
+        // if(n==cnt){
+        //     head=head->next;
+        //     return head;
+        // }
+        // temp=head;
+        // int postn=cnt-n+1; // node to be deleted 5-2+1
+        // cnt=1;
+        // ListNode* prev=NULL;
+        // while(temp!=NULL && cnt<postn){
+        //     if(cnt==postn){break;}
+        //     else{
+        //         prev=temp;
+        //         temp=temp->next;
+        //         cnt++;
+        //     }
+        // }
+        // prev->next=temp->next;
+        // delete temp;
+        // return head;
+        
+        // Method 2: Optimised Code
         if(head==NULL || head->next==NULL) return NULL;
-        int cnt=0;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            temp=temp->next;
-            cnt++;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        for(int i=0; i<n; i++){
+            fast=fast->next;
         }
-        if(n==cnt){
+        if(fast==NULL){
             head=head->next;
             return head;
         }
-        temp=head;
-        int postn=cnt-n+1; // node to be deleted 5-2+1
-        cnt=1;
-        ListNode* prev=NULL;
-        while(temp!=NULL && cnt<postn){
-            if(cnt==postn){break;}
-            else{
-                prev=temp;
-                temp=temp->next;
-                cnt++;
-            }
+        while(fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
         }
-        prev->next=temp->next;
-        delete temp;
+        slow->next=slow->next->next;
         return head;
     }
 };
