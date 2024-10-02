1823daef716eab3bd8436a92b4005759e56a9131
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-10-02 18:45:29 +0530

    Time: 13 ms (63.83%), Space: 19.5 MB (67.65%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -15,15 +15,41 @@ public:
         // }
         // return -1;
         
-        // Method 2: Better solution using hashing
-        map<int, int> mpp;
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            mpp[nums[i]]++;
+        
+        // Method 2: using hashing - (DONT USE THIS METHOD)
+        // int maxi=0;
+        // int n=nums.size();
+        // for(int i=0; i<n; i++){
+        //     maxi=max(maxi,nums[i]);
+        // }
+        // vector<int> hash(maxi+1,0); // maxi+1 is size since array has 0-based indexing
+        // for(int i=0; i<n; i++){
+        //     hash[nums[i]]++;
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(hash[nums[i]]==1) return nums[i];
+        // }
+        // return -1;
+        // NOTE: (IMPORTANT)
+        // The above method will not work when values are negative or numbers are very big i.e. to the order of 10^9 or 10^12 etc
+        
+        // Method 2: Better solution using maps
+        // map<int, int> mpp;
+        // int n=nums.size();
+        // for(int i=0; i<n; i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it:mpp){
+        //     if(it.second==1) return it.first;
+        // }
+        // return -1;
+        
+        
+        // Method 3: Using XOR
+        int xor1 = 0; // xor is a reserved keyword in c++
+        for(int i=0; i<nums.size(); i++){
+            xor1=xor1^nums[i];
         }
-        for(auto it:mpp){
-            if(it.second==1) return it.first;
-        }
-        return -1;
+        return xor1;
     }
 };
