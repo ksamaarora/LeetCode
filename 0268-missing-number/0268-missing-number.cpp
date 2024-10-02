06a9987b33168d333973316b5eaee36cf51e94f2
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-10-02 15:35:48 +0530

    Time: 15 ms (51.11%), Space: 20.8 MB (37.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -19,19 +19,19 @@ public:
         
         
         // Method 2: Better soln
-        int n=nums.size();
-        vector<int> hash(n+1,0); // declare hash of size n with all values 0
-        for(int i=0; i<n; i++){ // // Count occurrences of each number in nums
-            hash[nums[i]]++;
-        }
-        for(int i=0; i<=n; i++){ // loop till n (incl n)
-            if(hash[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        // int n=nums.size();
+        // vector<int> hash(n+1,0); // declare hash of size n with all values 0
+        // for(int i=0; i<n; i++){ // // Count occurrences of each number in nums
+        //     hash[nums[i]]++;
+        // }
+        // for(int i=0; i<=n; i++){ // loop till n (incl n)
+        //     if(hash[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return -1;
         
-        // Method 2: Optimal method TC:O(N) SC:O(1)
+        // Method 3: Optimal method TC:O(N) SC:O(1)
         // sum of first n natural numbers: n(n+1)/2
         // int n=nums.size();
         // int sum=(n*(n+1))/2;
@@ -40,5 +40,16 @@ public:
         //     sum_arr=sum_arr+nums[i];
         // }
         // return sum-sum_arr; // missing element
+        
+        // Method 4: Most Optimal solution
+        int n=nums.size();
+        int xor1=0, xor2=0;
+        for(int i=1; i<=n; i++){
+            xor1=xor1^i;
+        }
+        for(int i=0; i<n; i++){
+            xor2=xor2^nums[i];
+        }
+        return xor1^xor2;
     }
 };
