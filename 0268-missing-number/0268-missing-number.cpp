97434d73eb85a5b6bc5852ed5cbff69e47e44f26
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-10-02 14:31:28 +0530

    Time: 11 ms (81.57%), Space: 20.6 MB (91.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         // Method 1: Brute force TC:O(NxN) SC:O(1)
-        int n=nums.size();
-        for(int i=0; i<=n; i++){
-            bool found=false;
-            for(int j=0; j<n; j++){ // iterate over nums array to check if nums[j]==i somewhere or not 
-                if(nums[j]==i){
-                    found=true;
-                    break;
-                }
-            }
-            if(found==false){
-                return i;
-            }
-        }
-        return -1; // input is invalid
+        // int n=nums.size();
+        // for(int i=0; i<=n; i++){
+        //     bool found=false;
+        //     for(int j=0; j<n; j++){ // iterate over nums array to check if nums[j]==i somewhere or not 
+        //         if(nums[j]==i){
+        //             found=true;
+        //             break;
+        //         }
+        //     }
+        //     if(found==false){
+        //         return i;
+        //     }
+        // }
+        // return -1; // input is invalid
         
-        // Method 2: Optimal method
+        // Method 2: Optimal method TC:O(N) SC:O(1)
+        // sum of first n natural numbers: n(n+1)/2
+        int n=nums.size();
+        int sum=(n*(n+1))/2;
+        int sum_arr=0;
+        for(int i=0; i<n; i++){
+            sum_arr=sum_arr+nums[i];
+        }
+        return sum-sum_arr; // missing element
     }
 };
