a59f9ef6a4ec08fbd069e1cce1272a0b922023a5
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-10-04 20:56:58 +0530

    Time: 80 ms (40.58%), Space: 70.5 MB (72.10%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // Method 1: Brute force solution TC:O(N^3) SC:O(1)
+        // Time Limit Exceeded
+        // int n=nums.size();
+        // int largest=INT_MIN;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         int sum=0;
+        //         for(int k=i; k<=j; k++){
+        //             sum=sum+nums[k];
+        //         }
+        //         if(sum>largest) largest=sum;
+        //     }
+        // }
+        // return largest;
+        
+        // Method 2: Better Soln TC:O(N^2) SC:O(1)
+        // Time Limit Exceeded
+        // int n=nums.size();
+        // int maxi=INT_MIN;
+        // for(int i=0; i<n; i++){
+        //     int sum=0;
+        //     for(int j=i; j<n; j++){
+        //         sum+=nums[j];
+        //         maxi=max(sum,maxi);
+        //     }
+        // }
+        // return maxi;
+        
+        // Method 3: Optimal Solution
+        int sum=0;
+        int n=nums.size();
+        int maxi=INT_MIN;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            maxi=max(maxi,sum);
+            if(sum<0) sum=0;
+        }
+        return maxi;
+    }
+};
