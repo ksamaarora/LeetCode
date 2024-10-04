522415f246adab85540249872037f14545f692b1
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-10-04 12:40:23 +0530

    Time: 22 ms (25.62%), Space: 27.1 MB (49.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         // Method 1: Brute force soln TC:O(N^2) SC:O(1)
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            int cnt=0;
-            for(int j=0; j<n; j++){
-                if(nums[i]==nums[j]){
-                    cnt++;
-                }
-            }
-            if(cnt>(n/2)){
-                return nums[i];
-            }
-        }
-        return -1;
+        // int n=nums.size();
+        // for(int i=0; i<n; i++){
+        //     int cnt=0;
+        //     for(int j=0; j<n; j++){
+        //         if(nums[i]==nums[j]){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt>(n/2)){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
         
         // Method 2: Better soln: Using map 
         // TC:O(N log N) + O(N) and SC:O(N) in worst case
@@ -32,5 +32,29 @@ public:
         //     }
         // }
         // return -1;
+        
+        // Method 3: Optimal Solution
+        // MOORE's VOTING ALGORITHM
+        int n=nums.size();
+        int cnt=0;
+        int el;
+        for(int i=0; i<n; i++){
+            if(cnt==0){
+                el=nums[i];
+                cnt=1;
+            }
+            else if(nums[i]==el){
+                cnt++;
+            }
+            else{ cnt--; }
+        }
+        int cnt1=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==el){
+                cnt1++;
+            }
+        }
+        if(cnt1>(n/2)){return el;}
+        return -1;
     }
 };
