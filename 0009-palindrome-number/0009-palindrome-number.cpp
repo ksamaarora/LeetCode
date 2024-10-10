da827866b8bc93e96aea5903ef1e5fc60bc9a19e
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-10-11 00:44:55 +0530

    Time: 9 ms (47.00%), Space: 8.5 MB (36.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,49 @@
+// Method 1 Reverse number and compare
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if(x<0){return false;}
+//         int xcopy=x;
+//         long long revnum=0; // to avoid overflow condition change int to long long
+//         while(xcopy!=0){
+//             int lastdigit=xcopy%10;
+//             revnum=revnum*10 + lastdigit;
+//             xcopy=xcopy/10;
+//         }
+//         if(revnum==x){return true;}
+//         else{ return false;}
+//     }
+// };
+
+
+// Method 2: Comparing digits from left and right
+// class Solution{
+// public:
+//     bool isPalindrome(int x){
+//         if(x<0){return false;}
+//         int n=(int)log10(x)+1; // number of digits -> log10(1234)=3.09(int)=3 -> +1 = 4 
+//         int i=0;
+//         while(i<n/2){
+//             int left=(x/(int)pow(10,n-i-1))%10;
+//             int right=(x/(int)pow(10,i))%10;
+//             if(left!=right) return false;
+//         }
+//         return true;
+//     }
+// };
+
+// Method 3: 
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){return false;}
-        int xcopy=x;
-        long long revnum=0;
-        while(xcopy!=0){
-            int lastdigit=xcopy%10;
-            revnum=revnum*10 + lastdigit;
-            xcopy=xcopy/10;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        if(revnum==x){return true;}
-        else{ return false;}
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10; 
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 };
