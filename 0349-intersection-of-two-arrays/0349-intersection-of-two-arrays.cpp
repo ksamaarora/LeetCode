cd7374b48cead1f8b8a0f47f45b95895dc00698f
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-11-03 18:13:39 +0530

    Time: 1 ms (55.85%), Space: 13.1 MB (61.92%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,43 +3,48 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
         // Brute Force Method
-        vector<int> ans;
+        // TC: O(n1 x n2 x n2) SC: O(n2) + O(k) to return ans 
+//         vector<int> ans;
+//         int n1=nums1.size();
+//         int n2=nums2.size();
+//         vector<int> vis(n2,0); // Initialization imp
+//         for(int i=0; i<n1; i++){
+//             for(int j=0; j<n2; j++){
+//                 if(nums1[i]==nums2[j] && vis[j]==0){
+//                     ans.push_back(nums1[i]);
+//                     vis[j]=1;
+                    
+//                     // to avoid duplicates in intersection array
+//                     int val=nums1[i];
+//                     for(int k=0; k<n2; k++){
+//                         if(nums2[k]==val){
+//                             vis[k]=1;
+//                         }
+//                     }
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+        
+        // Better Method 
+        // TC:O(n1 x n2)
+        // SC:O(n2)
+        set<int> st; // set doesnt store duplicate values
         int n1=nums1.size();
         int n2=nums2.size();
         vector<int> vis(n2,0); // Initialization imp
         for(int i=0; i<n1; i++){
             for(int j=0; j<n2; j++){
                 if(nums1[i]==nums2[j] && vis[j]==0){
-                    ans.push_back(nums1[i]);
+                    st.insert(nums1[i]);
                     vis[j]=1;
-                    int val=nums1[i];
-                    for(int k=0; k<n2; k++){
-                        if(nums2[k]==val){
-                            vis[k]=1;
-                        }
-                    }
                     break;
                 }
             }
         }
+        // convert set to vector to return
+        vector<int> ans(st.begin(), st.end());
         return ans;
-        
-        // Better Method 
-        // set<int> st;
-        // int n1=nums1.size();
-        // int n2=nums2.size();
-        // vector<int> vis(n2,0); // Initialization imp
-        // for(int i=0; i<n1; i++){
-        //     for(int j=0; j<n2; j++){
-        //         if(nums1[i]==nums2[j] && vis[j]==0){
-        //             st.insert(nums1[i]);
-        //             vis[j]=1;
-        //             break;
-        //         }
-        //     }
-        // }
-        // // convert set to vector to return
-        // vector<int> ans(st.begin(), st.end());
-        // return ans;
     }
 };
