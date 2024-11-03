a3bec538beca6c29056891535149604ea60aa717
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-11-03 15:14:46 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (25.94%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,16 +1,48 @@
-class Solution {
+// Method 1: Brute force solution
+// TC: O(N x (N-1)) ~~ O(NxN)
+// SC:O(1)
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n=s.size();
+//         if(s==goal) return true;
+//         for(int i=0; i<n; i++){
+//             char firstElement= s[0];
+//             for(int j=1; j<n; j++){
+//                 s[j-1]=s[j];
+//             }
+//             s[n-1]=firstElement;
+//             if(s==goal) return true;
+//         }
+//         return false;
+//     }
+// };
+
+// Method 2: (Not goood SC and same TC)
+// TC: O(N + N^2) ~~ O(N^2)
+// SC: O(2N) for s concatenation & substring space is not counted as it is used temporarily and not stored
+class Solution{
 public:
-    bool rotateString(string s, string goal) {
+    bool rotateString(string s, string goal){
         int n=s.size();
-        if(s==goal) return true;
-        for(int i=0; i<n; i++){
-            char firstElement= s[0];
-            for(int j=1; j<n; j++){
-                s[j-1]=s[j];
-            }
-            s[n-1]=firstElement;
-            if(s==goal) return true;
+        if(n!=goal.size()) return false;
+        // concatenate s with itselse
+        s+=s; // O(N)
+        // for loop: O(N x (N+1)) ~~ O(N^2)
+        for(int i=0; i<=n; i++){ // O(N)
+            if(s.substr(i,n)==goal) return true; // O(N+1) - 0 and N inclusive for substring extraction
         }
         return false;
     }
 };
+
+// Method 3: Optimal Solution
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n = s.size();
+//         if (n != goal.size()) return false; // Check if the lengths of s and goal are equal
+//         s += s; // Concatenate s with itself
+//         return s.find(goal) != string::npos; // Check if goal is in the concatenated string
+//     }
+// };
