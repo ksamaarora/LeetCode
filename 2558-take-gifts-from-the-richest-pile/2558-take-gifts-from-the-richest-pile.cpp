fdd7f75bc8546769f74d8ccbde1c5073c12f829a
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-12-12 10:06:15 +0530

    Time: 415 ms (5.15%), Space: 13.5 MB (7.74%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -25,23 +25,44 @@
 // };
 
 
-// Better Solution 
-// TC: O(N) + O(klogN)
+// Better Solution - sorting
+// TC: O(k*n*logn)
 // SC: O(N)
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        priority_queue<int> maxHeap(gifts.begin(), gifts.end()); // O(N)
-        for(int i=0; i<k; i++){ // O(klogN)
-            int maxGift=maxHeap.top();
-            maxHeap.pop();
-            maxHeap.push(floor(sqrt(maxGift)));
+        int n=gifts.size();
+        vector<int> sortedGifts=gifts;
+        for(int i=0; i<k; i++){ // k times loop
+            sort(sortedGifts.begin(), sortedGifts.end()); // O(N*logN)
+            int maxElement=sortedGifts.back();
+            sortedGifts.back()=floor(sqrt(maxElement));
         }
         long long sum=0;
-        while(!maxHeap.empty()){ // O(N)
-            sum+=maxHeap.top();
-            maxHeap.pop();
+        for(int i=0; i<n; i++){ // O(N)
+            sum=sum+sortedGifts[i];
         }
         return sum;
     }
 };
+
+// Optimal Solution - priority_queue maxHeap
+// TC: O(N) + O(klogN)
+// SC: O(N)
+// class Solution {
+// public:
+//     long long pickGifts(vector<int>& gifts, int k) {
+//         priority_queue<int> maxHeap(gifts.begin(), gifts.end()); // O(N)
+//         for(int i=0; i<k; i++){ // O(klogN)
+//             int maxGift=maxHeap.top();
+//             maxHeap.pop();
+//             maxHeap.push(floor(sqrt(maxGift)));
+//         }
+//         long long sum=0;
+//         while(!maxHeap.empty()){ // O(N)
+//             sum+=maxHeap.top();
+//             maxHeap.pop();
+//         }
+//         return sum;
+//     }
+// };
