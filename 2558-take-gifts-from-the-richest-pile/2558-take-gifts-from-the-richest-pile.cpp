7b0174cae20ec43d03ac77c19c796c539e515e11
Ksama Arora <134195071+ksamaarora@users.noreply.github.com>
2024-12-12 09:32:15 +0530

    Time: 4 ms (31.50%), Space: 13.5 MB (7.74%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -1,24 +1,44 @@
 // Brute force solution 
 // TC: O(k)xO(n) + O(n) for printing sum
 // SC: O(1)
+// class Solution {
+// public:
+//     long long pickGifts(vector<int>& gifts, int k) {
+//         int n=gifts.size();
+//         for(int i=0; i<k; i++){
+//             int max=0;
+//             int index;
+//             for(int j=0; j<n; j++){
+//                 if(gifts[j]>max){
+//                     max=gifts[j];
+//                     index=j;
+//                 }
+//             }
+//             gifts[index]=sqrt(gifts[index]);
+//         }
+//         long long sum=0;
+//         for(int i=0; i<n; i++){
+//             sum=sum+gifts[i];
+//         }
+//         return sum;
+//     }
+// };
+
+
+// Better Solution 
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        int n=gifts.size();
+        priority_queue<int> maxHeap(gifts.begin(), gifts.end());
         for(int i=0; i<k; i++){
-            int max=0;
-            int index;
-            for(int j=0; j<n; j++){
-                if(gifts[j]>max){
-                    max=gifts[j];
-                    index=j;
-                }
-            }
-            gifts[index]=sqrt(gifts[index]);
+            int maxGift=maxHeap.top();
+            maxHeap.pop();
+            maxHeap.push(sqrt(maxGift));
         }
         long long sum=0;
-        for(int i=0; i<n; i++){
-            sum=sum+gifts[i];
+        while(!maxHeap.empty()){
+            sum+=maxHeap.top();
+            maxHeap.pop();
         }
         return sum;
     }
